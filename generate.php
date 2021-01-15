10174e356791f28ae40aa18cd2e4fc22487bc79b
rasko-lazic <raskolazic@gmail.com>
2021-01-15 12:59:09 +0100

    added escaping to xml header string

--- generate.php
+++ generate.php
@@ -9,12 +9,12 @@ $newIndex = fopen('index_new.php', 'w');
 fwrite($newIndex,
     "<?php header('Content-type: application/xml'); 
     echo <<<EOL
-    <?xml version="1.0" encoding="UTF-8"?>
-    <rss version="2.0"
-    xmlns:content="http://purl.org/rss/1.0/modules/content/"
-    xmlns:dc="http://purl.org/dc/elements/1.1/"
-    xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
-    xmlns:trackback="http://madskills.com/public/xml/rss/module/trackback/">
+    <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+    <rss version=\"2.0\"
+    xmlns:content=\"http://purl.org/rss/1.0/modules/content/\"
+    xmlns:dc=\"http://purl.org/dc/elements/1.1/\"
+    xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\"
+    xmlns:trackback=\"http://madskills.com/public/xml/rss/module/trackback/\">
     <channel>
     EOL;
     ?>"
